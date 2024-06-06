b6ede28dbde76c4b246aa50cfed1b4d43063c772
Larry Osterman <LarryOsterman@users.noreply.github.com>
2024-06-06 14:02:56 -0700

    more fixes

--- sdk/core/azure-core/src/http/reqwest/ReqwestCppClient/src/RustHttpTransport.cpp
+++ sdk/core/azure-core/src/http/reqwest/ReqwestCppClient/src/RustHttpTransport.cpp
@@ -3,12 +3,13 @@
 // information.
 
 #include "azure/core/http/reqwest_transport.hpp"
+#include "private/reqwest_http_transport_private.hpp"
 
 #include <utility>
 
 namespace Azure { namespace Core { namespace Http { namespace RustTransport {
 
-  std::string RustHttpResponse::ReasonPhrase() const
+  std::string _detail::RustHttpResponse::ReasonPhrase() const
   {
     switch (StatusCode())
     {
@@ -20,7 +21,7 @@ namespace Azure { namespace Core { namespace Http { namespace RustTransport {
         return "Unknown";
     }
   }
-  size_t RustTransport::RustBodyStream::OnRead(
+  size_t _detail::RustBodyStream::OnRead(
       uint8_t* buffer,
       size_t count,
       Azure::Core::Context const&)
@@ -37,7 +38,8 @@ namespace Azure { namespace Core { namespace Http { namespace RustTransport {
       // result_get_body_bytes returns nullptr if the stream is fully consumed.
       if (!m_currentBytes)
       {
-        _detail::AutoRustError rustError{_detail::RustInterop::runtime_context_get_error(m_runtimeContext.get())};
+        _detail::AutoRustError rustError{
+            _detail::RustInterop::runtime_context_get_error(m_runtimeContext.get())};
         _detail::RustString error{_detail::RustInterop::rust_error_get_message(rustError.get())};
         throw std::runtime_error(error);
       }
@@ -79,10 +81,73 @@ namespace Azure { namespace Core { namespace Http { namespace RustTransport {
 
   struct Initializer
   {
-
     Initializer() { _detail::RustInterop::initialize(); }
   };
 
+  std::unique_ptr<RawResponse> RawResponseFromRustResponse(
+      _detail::AutoRuntimeContext&& runtimeContext,
+      _detail::RustHttpResponse&& rustResponse)
+  {
+    auto version = rustResponse.Version();
+    auto response = std::make_unique<RawResponse>(
+        version.first, version.second, rustResponse.StatusCode(), rustResponse.ReasonPhrase());
+
+    // Transfer the headers to the raw result.
+    for (const auto& header : rustResponse.Headers())
+    {
+      response->SetHeader(header.HeaderName(), header.HeaderValue());
+    }
+
+    response->SetBodyStream(rustResponse.CreateBodyStream(std::move(runtimeContext)));
+
+    return response;
+  }
+
+  _detail::ReqwestHttpTransportImpl::ReqwestHttpTransportImpl()
+      : m_rustReqwestWrapper{RustInterop::reqwest_wrapper_new()}
+  {
+  }
+
+
+  std::unique_ptr<Azure::Core::Http::RawResponse> _detail::ReqwestHttpTransportImpl::Send(
+      Azure::Core::Http::Request& request,
+      Azure::Core::Context const& context)
+  {
+    AutoRuntimeContext runtimeContext{_detail::RustInterop::runtime_context_new()};
+
+    RustHttpRequest rustRequest{request, context};
+    AutoRustHttpResult rustHttpResult{_detail::RustInterop::reqwest_wrapper_send(
+        runtimeContext.get(), m_rustReqwestWrapper.get(), rustRequest.GetRustRequest())};
+
+    if (result_has_error(rustHttpResult.get()))
+    {
+      // Extract the error from the result and return it. This is a destructive operation, so
+      // release the pointer.
+      auto err = _detail::AutoRustError{result_extract_error(rustHttpResult.release())};
+
+      _detail::RustString message{rust_error_get_message(err.get())};
+      throw Azure::Core::Http::TransportException("Rust API Call failed: " + std::string(message));
+    }
+
+    _detail::AutoRustRawResponse rustRawResponse{result_extract_response(rustHttpResult.release())};
+
+    RustHttpResponse rustResponse{std::move(rustRawResponse)};
+    return RawResponseFromRustResponse(std::move(runtimeContext), std::move(rustResponse));
+  }
+
   static Initializer initializer{};
+
+    ReqwestHttpTransport::ReqwestHttpTransport()
+      : m_pImpl{std::make_unique<_detail::ReqwestHttpTransportImpl>()}
+  {
+  }
+  ReqwestHttpTransport::~ReqwestHttpTransport() {}
+
+  std::unique_ptr<Azure::Core::Http::RawResponse> ReqwestHttpTransport::Send(
+      Azure::Core::Http::Request& request,
+      Azure::Core::Context const& context)
+  {
+    return m_pImpl->Send(request, context);
+  }
 
 }}}} // namespace Azure::Core::Http::RustTransport
